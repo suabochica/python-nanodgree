3be6f539fe93fac8f0d79f3a8025d0c57808909e
Sergio Benítez <sergio.kun21@gmail.com>
2026-09-18 11:40:00 -0500

    fix: add lists sidebar and active_list context to template

--- packages/todoapp_crud/src/todoapp_crud/app.py
+++ packages/todoapp_crud/src/todoapp_crud/app.py
@@ -101,6 +101,69 @@ def get_list_todos(list_id):
     )
 
 
+@app.route("/lists/create", methods=["POST"])
+def create_list():
+    error = False
+    body = {}
+    try:
+        name = request.get_json()["name"]
+        todolist = TodoList(name=name)
+        db.session.add(todolist)
+        db.session.commit()
+        body["id"] = todolist.id
+        body["name"] = todolist.name
+    except ():
+        db.session.rollback()
+        error = True
+        print(sys.exc_info)
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify(body)
+
+
+@app.route("/lists/<list_id>/delete", methods=["DELETE"])
+def delete_list(list_id):
+    error = False
+    try:
+        list = TodoList.query.get(list_id)
+        for todo in list.todos:
+            db.session.delete(todo)
+
+        db.session.delete(list)
+        db.session.commit()
+    except ():
+        db.session.rollback()
+        error = True
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return jsonify({"success": True})
+
+
+@app.route("/lists/<list_id>/set-completed", methods=["POST"])
+def set_completed_list(list_id):
+    error = False
+    try:
+        list = TodoList.query.get(list_id)
+        for todo in list.todos:
+            todo.completed = True
+        db.session.commit()
+    except:
+        db.session.rollback()
+        error = True
+    finally:
+        db.session.close()
+    if error:
+        abort(500)
+    else:
+        return "", 200
+
+
 @app.route("/")
 def index():
     return redirect(url_for("get_list_todos", list_id=1))
@@ -108,4 +171,4 @@ def index():
 
 # always include this at the bottom of your code
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000)
+    app.run(host="0.0.0.0", port=3000, debut=true)
